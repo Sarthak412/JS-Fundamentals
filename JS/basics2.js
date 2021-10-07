fc8a4b7fe9a4e0b952b9344715ace3adde44e9ff
Sarthak Kamble <sarthakkamble412@gmail.com>
2021-10-07 22:19:43 +0530

    Day 7 JavaScript

---
+++ JS/basics2.js
@@ -0,0 +1,19 @@
+/*
+----------------------------------------
+    JavaScript Fundamentals Part-2
+----------------------------------------
+*/
+
+/* Activating Strict Mode */
+
+// To use strict mode in JS use "use strict at the start of the file"
+
+"use strict"; 
+ 
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive.");  
+
+// Outputs 'I can drive'
