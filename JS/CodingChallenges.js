ba14d565225b02bfe5e8fc0750eba4e97358a831
Sarthak Kamble <sarthakkamble412@gmail.com>
2021-10-13 22:51:08 +0530

    Day 10 JavaScript

--- JS/CodingChallenges.js
+++ JS/CodingChallenges.js
@@ -230,4 +230,55 @@ const checkWinner = function (averageShark, averageHammerHead){
 }
 
 checkWinner(scoreShark, scoreHammerHead);
+
+/**
+ * Coding challenge - 2
  
+ * Naruto is still building his tip calculator, using the same rules as 
+   before: Tip 15% of the bill if the bill value is between 50 and 300, 
+   and if the value is different, the tip is 20%.
+
+ 1. Write a function 'calcTip' that takes any bill value as an input
+    and returns the corrensponding tip, calculated based on the rules above
+    (you can check out the code from the first tip calculator challenge if
+    you need to). Use the function type you like the most. Test the 
+    function using a bill value of 100.
+ 
+ 2. And now let's use arrays. So create an array 'bills' containing the
+    test data below.
+
+ 3. Create an array 'tips' containing the tip value for each bill, calculated
+    from the function you created before.
+ 
+ 4. Bonus: Create an array 'total' containing the total values, so the bill + tip.
+
+ Test Data: 125, 555 and 44.
+*/
+
+
+const tipCalc = function (bill) {
+  // You can use the ternary operator to reduce the code
+  
+  /*
+  return bill >= 50 && bill <=300 ? bill * 0.15 : bill * 0.2;
+  */
+
+  if(bill >= 50 && bill <= 300){
+    return bill * 0.15;
+  }
+  else{
+    return bill * 0.2;
+  }
+  
+};
+
+const bills = [125, 555, 44];
+
+const tips = [tipCalc(bills[0]), tipCalc(bills[1]), tipCalc(bills[2])];
+console.log(bills, tips);
+
+// Bonus challenge
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
